b4bfc1df75808f4629c6caea6d93055ab38f58e7
Lee Seong Won <syanggu@naver.com>
2020-11-06 20:28:30 +0900

    Day 10

---
+++ Day9-10/src/hooks/useFavicon.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+
+const useFavicon = (initialFaviconUrl) => {
+  const [url, setUrl] = useState(initialFaviconUrl)
+
+  const setFavicon = (faviconUrl = initialFaviconUrl) => {
+    setUrl(faviconUrl)
+  }
+
+  useEffect(() => {
+    const link = document.head.querySelectorAll('link')[1]
+    link.type = 'image/x-icon'
+    link.rel = 'shortcut icon'
+    link.href = url
+  }, [url])
+
+  return setFavicon
+}
+
+export default useFavicon
